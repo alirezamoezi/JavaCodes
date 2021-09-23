bed477e74aaf1757dd465a40ef049f8ccf5bb006
Seyedalireza Moezi <smoezi@my.centennialcollege.ca>
2021-09-23 19:11:00 -0400

    first commit

---
+++ MortgageTracker/ProcessMortgage.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+
+public class ProcessMortgage {
+	
+	public static void main(String[] args)
+	{
+		Mortgage[] arrayOfMortgages = new Mortgage[2];
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the current interest rate.");
+		double currentRate = input.nextDouble();
+		
+		
+		
+	}
+
+}
